7a70dc034dff3a517ab586cafa66a470d750bc43
jovanna <ladybutterfly.fly@gmail.com>
2017-11-04 17:08:05 +0100

    backend-faza-I

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quote;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class QuoteController extends Controller
+{
+    public function postQuote(Request $request) {
+        $quote = new Quote();
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 201);
+
+    }
+
+    public function getQuotes() {
+        $quotes = Quote::all();
+        return response()->json(['quotes' => $quotes], 200);
+
+    }
+
+    // we will request both: body and request parameter which is IDs
+    public function putQuote(Request $request, $id) {
+        $quote = Quote::find($id);
+        if(! $quote) {
+            return response()->json(['message' => 'Document not found'], 404);
+        }
+        $quote->content = $request->input('content');
+        $quote->save();
+        return response()->json(['quote' => $quote], 200);
+    }
+
+    public function deleteQuote($id) {
+        $quote = Quote::find($id);
+        $quote->delete();
+        return response()->json(['message' => 'Quote deleted'], 200);
+    }
+}
